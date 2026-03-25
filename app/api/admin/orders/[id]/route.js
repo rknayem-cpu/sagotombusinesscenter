bae092bc07e0f3f8327e4993bdf83f3c41da177f
rknayem-cpu <nayemlx.mab@gmail.com>
2026-03-25 08:24:09 +0000

    ok chaeck

--- app/api/admin/orders/[id]/route.js
+++ app/api/admin/orders/[id]/route.js
@@ -1,4 +1,6 @@
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 import { cookies } from 'next/headers';
 import connectDB from '@/db';
 import Order from '@/models/Order';
